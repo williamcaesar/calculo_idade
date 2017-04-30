c55879dcdc41139d88922161d4b6a0e813fa59a3
Caesar <caesar@localhost.localdomain>
2017-04-29 21:32:07 -0300

    modified:   calculo_idade.py incluso função verificação do numero de dias do mês

--- calculo_idade.py
+++ calculo_idade.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python3
-x = 0
-meses_31 = [1,3,5,7,8,10,12]
-meses_30 = [4,6,9,11]
-meses_28 = [2]
+TRUE = 1
+FALSE = 0
 
 nome = input("Qual o seu nome? ")
 ano = input("Que ano você nasceu? ")
 mes = input("Que mês? ")
 dia = input("Que dia? ")
 idade = 2017-int(ano)
+
 mes_nome = {1:"Janeiro",
             2:"Fevereiro",
             3:"Março",
@@ -21,7 +20,27 @@ mes_nome = {1:"Janeiro",
             10:"Outubro",
             11:"Novembro",
             12:"Dezembro"}
-try:
-    print("Olá",nome,"você nasceu em",dia,"de",mes_nome[int(mes)],"de",ano,"então você tem",idade,"anos")
-except Exception as e:
-    print ("Dados invalidos!")
+
+def dia_mes(dia, mes):
+    meses_31 = [1,3,5,7,8,10,12]
+    meses_30 = [4,6,9,11]
+    meses_28 = [2]
+    if int(mes) in meses_31:
+        if int(dia) < 31:
+            return TRUE
+    elif int(mes) in meses_30:
+        if int(dia) < 30:
+            return TRUE
+    elif int(mes) in meses_30:
+        if int(dia) < 28:
+            return TRUE
+    else:
+        return FALSE
+
+if dia_mes(dia,mes):
+    try:
+        print("Olá",nome,"você nasceu em",dia,"de",mes_nome[int(mes)],"de",ano,"então você tem",idade,"anos")
+    except Exception as e:
+        print ("Dados invalidos!")
+else:
+    print ("Numero de dias ou mês não combinam")
